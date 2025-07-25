78bce3641051f286b889a834b08b7012c5a7b83c
Lukas Arnold <dhbw@arnold-lukas.de>
2025-07-25 21:52:48 +0200

    Add database creation and router

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 import os
 
+from app.database import Base, engine
+from app import router
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Kraftwerte",
 )
@@ -13,3 +18,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=FileResponse)
 async def read_root_frontend():
     return FileResponse(os.path.join("static", "index.html"))
+
+
+app.include_router(router.router)
